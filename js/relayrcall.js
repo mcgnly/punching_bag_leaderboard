2eb4ce558d96c9b5c84d3f5a634bb40fc94bbbe5
Katie McGinley <katie@mcgnly.com>
2016-01-21 17:51:31 +0100

    api login working score scale to be out of 10, reference to G's removed

--- js/relayrcall.js
+++ js/relayrcall.js
@@ -10,7 +10,7 @@ $( document ).ready(function () {
       deviceId: keys.DEVICE_ID,
       incomingData: function(data){
         console.log("data from device", data)
-        $(".high-score").text(data.readings[0].value + " Gs")
+        $(".high-score").text(data.readings[0].value *5/4)
       }
     });
 });
