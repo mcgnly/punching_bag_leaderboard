9364a04651409a8f27d89942000436bced7fbb9b
Katie McGinley <katie@mcgnly.com>
2016-01-21 16:53:37 +0100

    make api keys a JS object

--- js/relayrcall.js
+++ js/relayrcall.js
@@ -1,18 +1,13 @@
 $( document ).ready(function () {
 
-//javascript business
-
-//import api values
-//import api_keys.txt
-
     var relayr = RELAYR.init({
-      appId: APP_ID
+      appId: keys.APP_ID
     });
 
 
     relayr.devices().getDeviceData({
-      token: TOKEN,
-      deviceId: DEVICE_ID,
+      token: keys.TOKEN,
+      deviceId: keys.DEVICE_ID,
       incomingData: function(data){
         console.log("data from device", data)
         $(".high-score").text(data.readings[0].value + " Gs")
