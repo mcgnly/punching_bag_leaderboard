29b508dcad88679a3c62a2bb334dc2eeadd4d0ea
Katie McGinley <katie@mcgnly.com>
2016-01-22 15:38:05 +0100

    create object to hold names associated with score

--- js/relayrcall.js
+++ js/relayrcall.js
@@ -5,6 +5,7 @@ $( document ).ready(function () {
     });
 
     var your_score = 0;
+    var leaderboard = {};
 
 
     relayr.devices().getDeviceData({
@@ -12,8 +13,8 @@ $( document ).ready(function () {
       deviceId: keys.DEVICE_ID,
       incomingData: function(data){
         console.log("data from device", data)
-        var raw_score = text(data.readings[0].value* (5/4))
-        var your_score = raw_score.toFixed(1)
+        var raw_score = text(data.readings[0].value * (5/4))
+        your_score = raw_score.toFixed(1)
         $(".high-score").text(your_score)
       }
     });
@@ -23,13 +24,26 @@ $( document ).ready(function () {
 		var scores = $('.high-score').val();
 		$('<li>').text(input_name).appendTo('.names');
 		$('<li>').text(your_score).appendTo('.scores');
+		leaderboard[scores] = input_name;
+		const ordered = {};
+		Object.keys(leaderboard).sort().forEach(function(key) {
+  		ordered[key] = leaderboard[key];
 		$('.status-box').val('');
+		});
+		
 	});
 
-// $("div").data("stack", []); //This sets the data key "stack" to an empty array
-// $("div").data("stack").push(your_score); //This retrieves the array and pushes a number to the array
-// console.log($("div").data("stack")); //This prints the array
 
+// example of how to create an object that acts as a dictionary and add to it
+// var data = {
+//     'manager': ["Prateek","Rudresh","Prashant"], 
+//     'employee': ["namit","amit","sushil"], 
+//     'hr': ["priya"]
+// };
+// data["key3"] = "value3";
+
+
+// 
 // I want to have the button disabled if the text field empty, not working right now
 	// $('.status-box').keyup(function() {
 	// 	var name_length = $(this).val().length;
