f2a7f0dc1267e5beda5ce1cf30080ec843189b53
Katie McGinley <katie@mcgnly.com>
2016-01-26 15:42:29 +0100

    comments like a mother

--- js/relayrcall.js
+++ js/relayrcall.js
@@ -1,28 +1,35 @@
 $( document ).ready(function () {
 
-    // var relayr = RELAYR.init({
-    //   appId: keys.APP_ID
-    // });
+//connect to cloud
+    var relayr = RELAYR.init({
+      appId: keys.APP_ID
+    });
 
+//init some variables
     var your_score = "";
     var leaderboard = [];
     var scores = 1;
 
+//more connecting to cloud things
+    relayr.devices().getDeviceData({
+      token: keys.TOKEN,
+      deviceId: keys.DEVICE_ID,
+      incomingData: function(data){
+        console.log("data from device", data)
 
-    // relayr.devices().getDeviceData({
-    //   token: keys.TOKEN,
-    //   deviceId: keys.DEVICE_ID,
-    //   incomingData: function(data){
-    //     console.log("data from device", data)
-        // var raw_score = text(data.readings[0].value * (5/4))
-        var raw_score = 2.5647356567456 * 5/4
+    //real relayr cloud data, scaled to be out of 10
+        var raw_score = text(data.readings[0].value * (5/4))
+    //fake number to test rounding when not connected to cloud
+        // var raw_score = 2.5647356567456 * 5/4
+
+    //round scaled number to one decimal, output is a string  
        your_score = raw_score.toFixed(1)
+       //plug it into high-score div of front-end
         $(".high-score").text(your_score)
-        // uncomment me to get raw data from relayr
-        // $(".high-score").text(data.readings[0].value)
-      // }
-    // });
+      }
+    });
 
+//simulate a punch when the cloud isn't connected
   //   $('#punch-button').click(function() {
 		// //generate random number to simulate a punch
 		// your_score = Math.floor((Math.random() * 10) + 1);
@@ -30,27 +37,32 @@ $( document ).ready(function () {
 		// // console.log("punch value: ", your_score)
 		// });	
 
-// when you click the button:
+// when you click the save button:
 	$('#save-button').click(function() {
 		//it takes the name out of the box and the score,
 		var input_name = $('.status-box').val();
 		var scores = $('.high-score').text();
+		//throw it into an array to save
 		var name_score = [scores, input_name]
-		//clear out the old list
+		//clear out the old top-score list to redisplay later
 		$('.names').empty();
 
+		//puts the array into our list of name/score arrays
 		leaderboard.push(name_score)
+		//sorts it to ascending scores
 		ordered = leaderboard.sort();
 		console.log(JSON.stringify(ordered));
+		//clears out the old score as well
 		$('.status-box').val('');
 
-		// loop through the ordered index and make a list of them
+		// loop through the list of name/score arrays backwards, to get a DESCENDING order list
 		for (var index = ordered.length; index-- > 0; ) {
-			$('<li>').text(ordered[index][1]+ "   :   " + ordered[index][0]).appendTo('.names');
-		};
+			//shove the array with name and score into the 
+			$('<li>').text(ordered[index][1]+ "   :   " + ordered[index][0]).appendTo('.names-scores');
+			};
 
 	});	
-	});
+});
 
 // 
 // I want to have the button disabled if the text field empty, not working right now
