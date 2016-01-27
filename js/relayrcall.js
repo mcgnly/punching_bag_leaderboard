122054ca3763d23a7c13b446dc265c48c950e4c1
Katie McGinley <katie@mcgnly.com>
2016-01-27 15:51:58 +0100

    fixed weird leaderboard doubling

--- js/relayrcall.js
+++ js/relayrcall.js
@@ -44,8 +44,8 @@ $( document ).ready(function () {
 		//throw it into an array to save
 		var name_score = [scores, input_name]
 		//clear out the old top-score list to redisplay later
-		$('.names').empty();
-
+		$('.names-scores').empty();
+console.log($('.names-scores').text());
 		//puts the array into our list of name/score arrays
 		leaderboard.push(name_score)
 		//sorts it to ascending scores
