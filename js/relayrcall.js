ed134baa95d8002db965475666b30c1b5e35bbe8
Katie McGinley <katie@mcgnly.com>
2016-02-01 10:52:34 +0100

    take out punch button enable relayr cloud

--- js/relayrcall.js
+++ js/relayrcall.js
@@ -1,9 +1,9 @@
 $( document ).ready(function () {
 
 //connect to cloud
-    // var relayr = RELAYR.init({
-    //   appId: keys.APP_ID
-    // });
+    var relayr = RELAYR.init({
+      appId: keys.APP_ID
+    });
 
 //init some variables
     var your_score = "";
@@ -11,30 +11,30 @@ $( document ).ready(function () {
     var scores = 1;
 
 //more connecting to cloud things
-    // relayr.devices().getDeviceData({
-    //   token: keys.TOKEN,
-    //   deviceId: keys.DEVICE_ID,
-    //   incomingData: function(data){
-    //     console.log("data from device", data)
+    relayr.devices().getDeviceData({
+      token: keys.TOKEN,
+      deviceId: keys.DEVICE_ID,
+      incomingData: function(data){
+        console.log("data from device", data)
 
     //real relayr cloud data, scaled to be out of 10
-        // var raw_score = text(data.readings[0].value * (5/4))
+        var raw_score = text(data.readings[0].value * (5/4))
     //fake number to test rounding when not connected to cloud
         // var raw_score = 2.5647356567456 * 5/4
 
     //round scaled number to one decimal, output is a string  
-       // your_score = raw_score.toFixed(1)
+       your_score = raw_score.toFixed(1)
        // //plug it into high-score div of front-end
-       //  $(".high-score").text(your_score)
-    //   }
-    // });
+        $(".high-score").text(your_score)
+      }
+    });
 
-//simulate a punch when the cloud isn't connected
-    $('#punch-button').click(function() {
-		//generate random number to simulate a punch
-		your_score = Math.floor((Math.random() * 10));
-		$(".high-score").text(your_score)
-		});	
+// //simulate a punch when the cloud isn't connected
+//     $('#punch-button').click(function() {
+// 		//generate random number to simulate a punch
+// 		your_score = Math.floor((Math.random() * 10));
+// 		$(".high-score").text(your_score)
+// 		});	
 
 // when you click the save button:
 	$('#save-button').click(function() {
